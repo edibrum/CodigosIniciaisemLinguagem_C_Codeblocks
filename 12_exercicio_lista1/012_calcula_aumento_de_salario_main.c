e64646ba42fe8c86a764f28f3ce435253be0beb0
Edineia Brum (she/her/hers) <106160411+edibrum@users.noreply.github.com>
2022-08-26 11:07:41 -0300

    Renomeando arquivos 012

--- 12_exercicio_lista1/012_calcula_aumento_de_salario_main.c
+++ 12_exercicio_lista1/012_calcula_aumento_de_salario_main.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
-/* EXERCÕCIO 12: FaÁa um algoritmo para ler o sal·rio de um funcion·rio e aument·-Io em 15%. ApÛs o aumento, desconte 8%
-de impostos. Imprima o sal·rio inicial, o sal·rio com o aumento e o sal·rio final. */
+/* EXERC√çCIO 12: Fa√ßa um algoritmo para ler o sal√°rio de um funcion√°rio e aument√°-Io em 15%. Ap√≥s o aumento, desconte 8%
+de impostos. Imprima o sal√°rio inicial, o sal√°rio com o aumento e o sal√°rio final. */
 
 int main()
 {
     float a,b,aumento,desconto;
 
-    /* Pede para o usu·rio digitar o sal·rio: */
+    /* Pede para o usu√°rio digitar o sal√°rio: */
     printf("Digite o salario: \n");
     scanf("%f",&a);
 
-    /* C·lculos: */
+    /* C√°lculos: */
     aumento=a*0.15;
     desconto=(a+aumento)*0.08;
     b=(a+aumento)-desconto;
 
-    /*Exibe o resultado para o usu·rio: */
+    /*Exibe o resultado para o usu√°rio: */
     printf("\n Salario inicial: R$ %.2f .\n Salario com aumento: R$ %.2f .\n Salario final: R$ %.2f .\n",a,a+aumento,b);
     return 0;
 }
