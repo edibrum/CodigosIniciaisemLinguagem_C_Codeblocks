6ee0e3dfdd04aafb3385fe05cbc44cbc3690d053
Edineia Brum (she/her/hers) <106160411+edibrum@users.noreply.github.com>
2022-08-26 09:54:26 -0300

    Renomeando arquivos 004

--- 04_exercicio_lista1/004_calcula_dias_de_vida_da_pessoa_main.c
+++ 04_exercicio_lista1/004_calcula_dias_de_vida_da_pessoa_main.c
@@ -1,26 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
-/* EXERCÕCIO 4: Escreva um algoritmo para ler o nome e a idade de uma pessoa, e exibir quantos dias de vida ela possui.
+/* EXERC√çCIO 4: Escreva um algoritmo para ler o nome e a idade de uma pessoa, e exibir quantos dias de vida ela possui.
 Considere sempre anos completos, e que um ano possui 365 dias. Ex: uma pessoa com 19 anos possui 6935 dias de vida;
-veja um exemplo de saÌda: MARIA, VOC  J¡ VIVEU 6935 DIAS */
+veja um exemplo de sa√≠da: MARIA, VOC√ä J√Å VIVEU 6935 DIAS */
 
 int main()
 {
    char nome[20];
    int ano,r;
 
-    /*Pede o nome do usu·rio: */
+    /*Pede o nome do usu√°rio: */
     printf("Digite o seu primeiro nome: \n");
     scanf("%s",nome);
 
-    /*Pede a idade do usu·rio: */
+    /*Pede a idade do usu√°rio: */
     printf("Digite a sua idade: \n");
     scanf("%d",&ano);
 
-    /*C·lculo: */
+    /*C√°lculo: */
     r=ano*365;
 
-    /*Exibe o resultado para o usu·rio: */
+    /*Exibe o resultado para o usu√°rio: */
     printf("\n \n Voce, %s, ja viveu aroximadamente %d dias de vida. \n \n",nome,r);
 
     return 0;
