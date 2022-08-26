483bebbddaebd0437f6aa9eb4e52f4f79b05e7d0
Edineia Brum (she/her/hers) <106160411+edibrum@users.noreply.github.com>
2022-08-26 11:33:27 -0300

    Renomeando arquivos 021

--- 21_exercicio_lista1/021_calcula_litros_comprados_main.c
+++ 21_exercicio_lista1/021_calcula_litros_comprados_main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-/* EXERCÍCIO 21: A fábrica de refrigerantes Meia-Cola vende seu produto em três formatos: lata de 350 ml, garrafa de 600 ml e
-garrafa de 2 litros. Se um comerciante compra uma determinada quantidade de cada formato, faça um algoritmo para calcular
+/* EXERCÃCIO 21: A fÃ¡brica de refrigerantes Meia-Cola vende seu produto em trÃªs formatos: lata de 350 ml, garrafa de 600 ml e
+garrafa de 2 litros. Se um comerciante compra uma determinada quantidade de cada formato, faÃ§a um algoritmo para calcular
 quantos litros de refrigerante ele comprou. */
 
 int main()
@@ -9,7 +9,7 @@ int main()
     int latas,garrafa600,garrafa2l;
     float total;
 
-    /* Pede ao usuário que digite a quantidade de cada formato: */
+    /* Pede ao usuÃ¡rio que digite a quantidade de cada formato: */
     printf("Digite a quantidade de latas de 350ml: \n");
     scanf("%d",&latas);
 
@@ -19,10 +19,10 @@ int main()
     printf("Digite a quantidade de garrafas de 2 litros: \n");
     scanf("%d",&garrafa2l);
 
-    /* Cálculo: */
+    /* CÃ¡lculo: */
     total=(latas*0.350)+(garrafa600*0.6)+(garrafa2l*2);
 
-    /* Exibe o resultado ao usuário: */
+    /* Exibe o resultado ao usuÃ¡rio: */
     printf("O total de refrigerante comprado foi de %.3f litros: \n",total);
 
 
