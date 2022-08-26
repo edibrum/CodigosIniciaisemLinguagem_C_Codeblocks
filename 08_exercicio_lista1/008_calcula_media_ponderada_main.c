efc3e8328ed7dc528be4dcc851b077cfaa6d9e14
Edineia Brum (she/her/hers) <106160411+edibrum@users.noreply.github.com>
2022-08-26 09:59:02 -0300

    Renomeando arquivos 008

--- 08_exercicio_lista1/008_calcula_media_ponderada_main.c
+++ 08_exercicio_lista1/008_calcula_media_ponderada_main.c
@@ -1,28 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
-/* EXERCÕCIO 8: FaÁa um algoritmo para ler trÍs notas de um aluno em uma disciplina e imprimir a sua mÈdia ponderada (as
+/* EXERC√çCIO 8: Fa√ßa um algoritmo para ler tr√™s notas de um aluno em uma disciplina e imprimir a sua m√©dia ponderada (as
 notas tem pesos respectivos de 1, 2 e 3).*/
 
 int main()
 {
     float a,b,c,m;
 
-    /* Pede para o usu·rio inserir a nota 1: */
+    /* Pede para o usu√°rio inserir a nota 1: */
     printf("Digite a nota 1: \n");
     scanf("%f",&a);
 
-      /* Pede para o usu·rio inserir a nota 2: */
+      /* Pede para o usu√°rio inserir a nota 2: */
     printf("Digite a nota 2: \n");
     scanf("%f",&b);
 
-      /* Pede para o usu·rio inserir a nota 3: */
+      /* Pede para o usu√°rio inserir a nota 3: */
     printf("Digite a nota 3: \n");
     scanf("%f",&c);
 
-    /* C·lculo: */
+    /* C√°lculo: */
     m=a*0.1+b*0.2+c*0.3;
 
-    /*Exibe o resultado para o usu·rio: */
+    /*Exibe o resultado para o usu√°rio: */
     printf("\n A media ponderada e: %.2f .\n \n",m);
 
     return 0;
