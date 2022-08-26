c5ad3d33afbc56cd43b5402db70160e276e295f1
Edineia Brum (she/her/hers) <106160411+edibrum@users.noreply.github.com>
2022-08-26 11:41:53 -0300

    Renomeando arquivos 028

--- 28_exercicio_lista1/028_calcula_media_ponderada_main.c
+++ 28_exercicio_lista1/028_calcula_media_ponderada_main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-/* EXERCÕCIO 28: FaÁa um algoritmo que receba duas notas, calcule e mostre a mÈdia ponderada dessas notas, considerando
+/* EXERC√çCIO 28: Fa√ßa um algoritmo que receba duas notas, calcule e mostre a m√©dia ponderada dessas notas, considerando
 peso 2(4) para a primeira nota e peso 3(6) para a segunda nota.
 Obs.: alterei para 4 e 6 para fechar os 10. */
 
@@ -8,7 +8,7 @@ int main()
 {
     float n1,n2,mediaponderada;
 
-    /*Pede para o usu·rio digitar as notas e recebe os valores:*/
+    /*Pede para o usu√°rio digitar as notas e recebe os valores:*/
     printf("Digite a primeira nota:\n");
     scanf("%f",&n1);
 
@@ -18,7 +18,7 @@ int main()
     /*Calculo:*/
     mediaponderada=n1*0.4+n2*0.6;
 
-    /*Retorna a media para o usu·rio:*/
+    /*Retorna a media para o usu√°rio:*/
     printf("A media ponderada e: %.2f .\n",mediaponderada);
 
     return 0;
