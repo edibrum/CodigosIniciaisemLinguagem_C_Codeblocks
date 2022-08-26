6961b87fbaea14cca8b8bae044cf95d7b6a0d59e
Edineia Brum (she/her/hers) <106160411+edibrum@users.noreply.github.com>
2022-08-26 12:51:19 -0300

    Renomeando arquivos 039

--- 39_exercicio_lista1/039_calcula_pagamento_contas_vencidas_main.c
+++ 39_exercicio_lista1/039_calcula_pagamento_contas_vencidas_main.c
@@ -1,28 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
-/* EXERCÕCIO 39: Jo„o recebeu seu sal·rio de X e precisa pagar duas contas (c1 e c2) que
-est„o atrasadas. Como as contas est„o atrasadas, Jo„o ter· de pagar multa de 2% sobre cada conta. FaÁa
-um algoritmo que calcule e mostre quanto restar· do sal·rio do Jo„o. */
+/* EXERC√çCIO 39: Jo√£o recebeu seu sal√°rio de X e precisa pagar duas contas (c1 e c2) que
+est√£o atrasadas. Como as contas est√£o atrasadas, Jo√£o ter√° de pagar multa de 2% sobre cada conta. Fa√ßa
+um algoritmo que calcule e mostre quanto restar√° do sal√°rio do Jo√£o. */
 
 int main()
 {
     float salario,c1,c2,restante;
 
-    /*Pede ao usu·rio que digite o sal·rio*/
+    /*Pede ao usu√°rio que digite o sal√°rio*/
     printf("Digite o salario em reais: \n");
     scanf("%f",&salario);
 
-    /*Pede ao usu·rio que digite o valor das contas*/
+    /*Pede ao usu√°rio que digite o valor das contas*/
     printf("Digite o valor da conta 1 em reais: \n");
     scanf("%f",&c1);
 
     printf("Digite o valor da conta 2 em reais: \n");
     scanf("%f",&c2);
 
-    /*C·lculo:*/
+    /*C√°lculo:*/
     restante=salario-(c1*1.02+c2*1.02);
 
-    /*Exibe o resultado para o usu·rio:*/
+    /*Exibe o resultado para o usu√°rio:*/
     printf("Apos pagar as duas contas, sobrara: R$ %.2f reais do seu salario.",restante);
 
     return 0;
