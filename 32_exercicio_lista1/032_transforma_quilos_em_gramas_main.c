b43cb6a7a16b49706bba1c0552bb90560574f340
Edineia Brum (she/her/hers) <106160411+edibrum@users.noreply.github.com>
2022-08-26 11:50:52 -0300

    Renomeando arquivos 032

--- 32_exercicio_lista1/032_transforma_quilos_em_gramas_main.c
+++ 32_exercicio_lista1/032_transforma_quilos_em_gramas_main.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
-/* EXERCÕCIO 32: FaÁa um algoritmo que receba o peso de uma pessoa em quilos, calcule e mostre esse peso em gramas. */
+/* EXERC√çCIO 32: Fa√ßa um algoritmo que receba o peso de uma pessoa em quilos, calcule e mostre esse peso em gramas. */
 
 int main()
 {
     float q,g;
 
-    /*Pede ao usu·rio que digite o peso em quilos:*/
+    /*Pede ao usu√°rio que digite o peso em quilos:*/
     printf("Digite seu peso em quilos: \n");
     scanf("%f",&q);
 
-    /*C·lculo:*/
+    /*C√°lculo:*/
     g=q*1000;
 
     /*Exibe o resultado:*/
