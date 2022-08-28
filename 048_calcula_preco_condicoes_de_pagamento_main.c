55ffbda344d9707941264f874581f64010420576
Edineia Brum (she/her/hers) <106160411+edibrum@users.noreply.github.com>
2022-08-28 15:19:34 -0300

    Renomeando arquivos 048

--- 048_calcula_preco_condicoes_de_pagamento_main.c
+++ 048_calcula_preco_condicoes_de_pagamento_main.c
@@ -1,28 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 /*LISTA 2:
-EXERCÕCIO 8: Elabore um algoritmo que calcule o que deve ser pago por um produto, considerando o preÁo normal de etiqueta e a escolha
-da condiÁ„o de pagamento. Utilize os cÛdigos da tabela a seguir para ler qual a condiÁ„o de pagamento escolhida e efetuar o c·lculo
-adequado. CÛdigo CondiÁ„o de pagamento
-a) ¿ vista em dinheiro ou cheque, recebe 10% de desconto
-b) ¿ vista no cart„o de crÈdito, recebe 15% de desconto
-c) Em duas vezes no cartao da loja, preÁo normal de etiqueta sem juros
-d) Em duas vezes no cartao de credito, preÁo normal de etiqueta mais juros de 10% */
+EXERC√çCIO 8: Elabore um algoritmo que calcule o que deve ser pago por um produto, considerando o pre√ßo normal de etiqueta e a escolha
+da condi√ß√£o de pagamento. Utilize os c√≥digos da tabela a seguir para ler qual a condi√ß√£o de pagamento escolhida e efetuar o c√°lculo
+adequado. C√≥digo Condi√ß√£o de pagamento
+a) √Ä vista em dinheiro ou cheque, recebe 10% de desconto
+b) √Ä vista no cart√£o de cr√©dito, recebe 15% de desconto
+c) Em duas vezes no cartao da loja, pre√ßo normal de etiqueta sem juros
+d) Em duas vezes no cartao de credito, pre√ßo normal de etiqueta mais juros de 10% */
 
 int main()
 {
     float preco,total;
     int opcao;
 
-    /*Pede ao usu·rio que digite o preco normal de etiqueta:*/
+    /*Pede ao usu√°rio que digite o preco normal de etiqueta:*/
     printf("Digite o preco normal de etiqueta em reais: \n");
     scanf("%f",&preco);
 
-    /*Pede ao usu·rio que digite a condiÁ„o:*/
+    /*Pede ao usu√°rio que digite a condi√ß√£o:*/
     printf("Digite a opcao: \n 1 - A vista em dinheiro ou cheque (10%% de desconto) \n 2 - A vista no cartao de credito (15%% de desconto) \n 3 - Em duas vezes no cartao da loja (preco da etiqueta) \n 4 - Em duas vezes no cartao de credito (10%% de acrescimo \n");
     scanf("%d",&opcao);
 
-    /*Analisa a situaÁ„o e calcula:*/
+    /*Analisa a situa√ß√£o e calcula:*/
     if (opcao==1)
         total=preco*0.9;
     else if (opcao==2)
