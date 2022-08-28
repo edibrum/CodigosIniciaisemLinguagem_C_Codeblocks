ac9a09f1a9c20840b268c7baba884cbd55c61936
Edineia Brum (she/her/hers) <106160411+edibrum@users.noreply.github.com>
2022-08-28 15:16:15 -0300

    Renomeando arquivos 044

--- 044_verifica_numero_positivo_ou_negativo_e_opera_main.c
+++ 044_verifica_numero_positivo_ou_negativo_e_opera_main.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 /*LISTA 2:
- EXERCÍCIO 4: Encontrar o dobro de um número caso ele seja positivo e o seu triplo caso seja negativo, imprimindo o resultado. */
+ EXERCÃCIO 4: Encontrar o dobro de um nÃºmero caso ele seja positivo e o seu triplo caso seja negativo, imprimindo o resultado. */
 
 int main()
 {
     int a;
 
-    /*Explica o programa para o usuário:*/
+    /*Explica o programa para o usuÃ¡rio:*/
     printf("Ola! Voce deve digitar um numero e apresentaremos o resultado: \n o DOBRO para um numero positivo e o TRIPLO para um numero negativo.\n \n");
 
-    /*Pede ao usuário que digite o valor:*/
+    /*Pede ao usuÃ¡rio que digite o valor:*/
     printf("Digite o numero: \n");
     scanf("%d",&a);
 
-    /*Verifica as situacoes e exibe o resultado para o usuário:*/
+    /*Verifica as situacoes e exibe o resultado para o usuÃ¡rio:*/
     if (a>=0)
         printf("O numero %d e positivo e o seu dobro e: %d",a,2*a);
     else
