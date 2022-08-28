c6fe53eef0db8b25d1c49c14b4e1e775ae1e2a5b
Edineia Brum (she/her/hers) <106160411+edibrum@users.noreply.github.com>
2022-08-28 15:15:24 -0300

    Renomeando arquivos 043

--- 043_verifica_valores_IGUAIS_soma_ou_multiplica_main.c
+++ 043_verifica_valores_IGUAIS_soma_ou_multiplica_main.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 /*LISTA 2:
- EXERCÕCIO 3: FaÁa um algoritmo que leia dois valores inteiros A e B se os valores forem iguais dever· se somar os dois,
-caso contr·rio multiplique A por B. Ao final, apresente o resultado. */
+ EXERC√çCIO 3: Fa√ßa um algoritmo que leia dois valores inteiros A e B se os valores forem iguais dever√° se somar os dois,
+caso contr√°rio multiplique A por B. Ao final, apresente o resultado. */
 
 int main()
 {
     int a,b;
 
-    /*Explica o programa para o usu·rio:*/
+    /*Explica o programa para o usu√°rio:*/
     printf("Ola! Voce deve digitar dois numeros inteiros e apresentaremos o resultado: \n SOMA para numeros iguais e MULTIPLICACAO para numeros diferentes.\n \n");
-    /*Pede ao usu·rio que digite os valores:*/
+    /*Pede ao usu√°rio que digite os valores:*/
     printf("Digite o primeiro numero: \n");
     scanf("%d",&a);
     printf("Digite o segundo numero: \n");
     scanf("%d",&b);
 
-    /*Verifica as situacoes e exibe o resultado para o usu·rio:*/
+    /*Verifica as situacoes e exibe o resultado para o usu√°rio:*/
     if (a==b)
         printf("O resultado e: %d + %d = %d",a,b,a+b);
     else
