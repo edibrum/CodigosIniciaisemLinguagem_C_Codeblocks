ea27c1ef4fe21a1c6a910d577650fd3771f2a4d9
Edineia Brum (she/her/hers) <106160411+edibrum@users.noreply.github.com>
2022-08-28 15:30:16 -0300

    Renomeando arquivos 056

--- 056_verifica_saldo_medio_e_credito_disponibilizado_main.c
+++ 056_verifica_saldo_medio_e_credito_disponibilizado_main.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 /*LISTA 2:
-EXERCÕCIO 16: Um banco conceder· um crÈdito especial aos seus clientes, vari·vel com o saldo mÈdio no ˙ltimo ano. FaÁa
-um algoritmo que leia o saldo mÈdio de um cliente e calcule o valor do crÈdito de acordo com a tabela abaixo.
-Mostre uma mensagem informando o saldo mÈdio e o valor do crÈdito.
+EXERC√çCIO 16: Um banco conceder√° um cr√©dito especial aos seus clientes, vari√°vel com o saldo m√©dio no √∫ltimo ano. Fa√ßa
+um algoritmo que leia o saldo m√©dio de um cliente e calcule o valor do cr√©dito de acordo com a tabela abaixo.
+Mostre uma mensagem informando o saldo m√©dio e o valor do cr√©dito.
 */
 
 int main()
