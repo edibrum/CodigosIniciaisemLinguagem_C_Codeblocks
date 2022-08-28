b5a1fcfeb8f922c755cff697279fa8b94340893b
Edineia Brum (she/her/hers) <106160411+edibrum@users.noreply.github.com>
2022-08-28 15:25:40 -0300

    Renomeando arquivos 052

--- 052_verifica_lados_TRIANGULO_e_classifica_tipo_main.c
+++ 052_verifica_lados_TRIANGULO_e_classifica_tipo_main.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 /*LISTA 2:
-EXERCÕCIO 12: Dados trÍs valores X, Y e Z, verificar se eles podem ser os comprimentos dos lados de um tri‚ngulo e, se
-forem verificar se È um tri‚ngulo equil·tero, isÛsceles ou escalenos. Se eles n„o formarem um tri‚ngulo, escrever a mensagem.
+EXERC√çCIO 12: Dados tr√™s valores X, Y e Z, verificar se eles podem ser os comprimentos dos lados de um tri√¢ngulo e, se
+forem verificar se √© um tri√¢ngulo equil√°tero, is√≥sceles ou escalenos. Se eles n√£o formarem um tri√¢ngulo, escrever a mensagem.
 Considere as seguintes propriedades:
-a. O comprimento de cada lado em um tri‚ngulo È menor que a soma dos outros dois lados;
-b. Equil·teros: tem os comprimentos dos trÍs lados iguais;
-c. IsÛsceles: tem os comprimentos de dois lados iguais;
-d. Escaleno: tem os comprimentos dos trÍs lados diferentes.
+a. O comprimento de cada lado em um tri√¢ngulo √© menor que a soma dos outros dois lados;
+b. Equil√°teros: tem os comprimentos dos tr√™s lados iguais;
+c. Is√≥sceles: tem os comprimentos de dois lados iguais;
+d. Escaleno: tem os comprimentos dos tr√™s lados diferentes.
 */
 int main()
 {
     float a,b,c;
 
-    /*Pede que o usu·rio digite os dados:*/
+    /*Pede que o usu√°rio digite os dados:*/
     printf("Digite a medida do lado A do triangulo: \n");
     scanf("%f",&a);
 
