b9f0c158b7b72789919255634f33aa7a8fbf6d16
Edineia Brum (she/her/hers) <106160411+edibrum@users.noreply.github.com>
2022-08-30 10:08:52 -0300

    Renomeando arquivos 069

--- 069_le_20_numeros_e_confere_tres_intervalos_main.c
+++ 069_le_20_numeros_e_confere_tres_intervalos_main.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 /*LISTA 3
-12) Faça um algoritmo que leia 20 números e, ao final, escreva quantos:
->>> estão entre 0 e 100
->>> quantos estão entre 101 e 200 e
->>> quantos são maiores de 200
+12) FaÃ§a um algoritmo que leia 20 nÃºmeros e, ao final, escreva quantos:
+>>> estÃ£o entre 0 e 100
+>>> quantos estÃ£o entre 101 e 200 e
+>>> quantos sÃ£o maiores de 200
 */
 
 int main()
 {
-    /* Declara uma três variáveis acumuladoras que vão contar quantos números estão em cada intervalo, e a variável número que vai receber cada valor digitado: */
+    /* Declara uma trÃªs variÃ¡veis acumuladoras que vÃ£o contar quantos nÃºmeros estÃ£o em cada intervalo, e a variÃ¡vel nÃºmero que vai receber cada valor digitado: */
     int num_0a100=0,num_101a200=0,num_maior201=0,numero;
 
 
-    /* Pede ao usuário que digite os 20 números: */
+    /* Pede ao usuÃ¡rio que digite os 20 nÃºmeros: */
     for (int i=1;i<=5;i++)
     {
         printf("Digite o numero %d: \n",i);
@@ -30,7 +30,7 @@ int main()
 
     }
 
-    /* Retorna o resultado para o usuário: */
+    /* Retorna o resultado para o usuÃ¡rio: */
     printf("Dentre os 20 numeros que voce digitou: \n");
     printf("%d numero(s) esta/estao entre 0 e 100. \n",num_0a100);
     printf("%d numero(s) esta/estao entre 101 e 200. \n",num_101a200);
