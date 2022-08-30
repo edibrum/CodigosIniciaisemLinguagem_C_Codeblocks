f80bedc79fba228b48b5e5f94db827f1b8ae450a
Edineia Brum (she/her/hers) <106160411+edibrum@users.noreply.github.com>
2022-08-30 09:56:35 -0300

    Renomeando arquivos 059

--- 059_calcula_somatorio_intervalo_main.c
+++ 059_calcula_somatorio_intervalo_main.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 /*LISTA 3
-2) Escreva um algoritmo que calcule o somatÛrio dos n˙meros de um intervalo informado pelo usu·rio. */
+2) Escreva um algoritmo que calcule o somat√≥rio dos n√∫meros de um intervalo informado pelo usu√°rio. */
 int main()
 {
-    /*Cria as vari·veis: */
+    /*Cria as vari√°veis: */
     int n1,n2,somatorio;
 
-    /*Pede que o usu·rio digite os valores para as vari·veis de entrada: */
+    /*Pede que o usu√°rio digite os valores para as vari√°veis de entrada: */
     printf("Digite o numero inicial do intervalo: \n");
     scanf("%d",&n1);
     somatorio=0;
