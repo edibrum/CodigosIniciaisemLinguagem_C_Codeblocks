e1a02fbe6f2c65a4cf7c92e057fbff198dc3fc06
Edineia Brum (she/her/hers) <106160411+edibrum@users.noreply.github.com>
2022-08-30 09:58:32 -0300

    Renomeando arquivos 061

--- 061_laco_repeticao_simples_Nvezes_main.c
+++ 061_laco_repeticao_simples_Nvezes_main.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 /*LISTA 3
-4) Leia o nome de um usu·rio e um n˙mero N e escreva o nome dele na tela N vezes. */
+4) Leia o nome de um usu√°rio e um n√∫mero N e escreva o nome dele na tela N vezes. */
 
 int main()
 {
     int n=0,vezes;
     char nome[40];
 
-    /* Pede ao usu·rio que digite o nome: */
+    /* Pede ao usu√°rio que digite o nome: */
     printf("Digite seu nome completo (para espacos use '_': \n");
     scanf(" %s",nome);
 
-    /* Pede ao usu·rio que digite o numero de vezes: */
+    /* Pede ao usu√°rio que digite o numero de vezes: */
     printf("Digite o numero de vezes a imprimir o seu nome: \n");
     scanf("%d",&vezes);
 
-    /* Mostra o nome do usu·rio 10 vezes: */
+    /* Mostra o nome do usu√°rio 10 vezes: */
     for (int i=1; i<=vezes; i++)
     {
         n=n+1;
