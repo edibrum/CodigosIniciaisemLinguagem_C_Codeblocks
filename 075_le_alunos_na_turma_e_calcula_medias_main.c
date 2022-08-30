70a2986a10df0b04c5a84f0d50e346f681a66bb0
Edineia Brum (she/her/hers) <106160411+edibrum@users.noreply.github.com>
2022-08-30 10:47:02 -0300

    Renomeando arquivos 075

--- 075_le_alunos_na_turma_e_calcula_medias_main.c
+++ 075_le_alunos_na_turma_e_calcula_medias_main.c
@@ -3,24 +3,24 @@
 #include <locale.h>
 /*LISTA 3
 18) Escreva um algoritmo que leia a quantidade de alunos em uma turma. Em seguida:
-a. para cada aluno, leia a informaÁ„o de suas 2 notas, calcule a mÈdia e informe se o
-b. aluno est· aprovado ou reprovado. O aluno est· aprovado apenas se a sua mÈdia for maior ou igual a 6. */
+a. para cada aluno, leia a informa√ß√£o de suas 2 notas, calcule a m√©dia e informe se o
+b. aluno est√° aprovado ou reprovado. O aluno est√° aprovado apenas se a sua m√©dia for maior ou igual a 6. */
 
 int main()
 {
     /* Para poder usar acentos e etc.:*/
     setlocale(LC_ALL, "Portuguese");
 
-    /* Declara as vari·veis: */
+    /* Declara as vari√°veis: */
     int quant_alunos;
     float nota1,nota2,media;
 
-    /* Pede ao usu·rio que digite as vari·veis: */
+    /* Pede ao usu√°rio que digite as vari√°veis: */
     printf("Digite a quantidade de alunos:");
     scanf("%d",&quant_alunos);
     printf("\n ******************************** \n");
 
-    /* Opera e mostra os resultados ao usu·rio: */
+    /* Opera e mostra os resultados ao usu√°rio: */
     for (int i=1;i<=quant_alunos;i++){
         printf("\n Digite a primeira nota d@ alun@ %d: ",i);
         scanf("%f",&nota1);
@@ -30,12 +30,12 @@ int main()
         media=(nota1+nota2)/2;
 
         if (media>=60.00){
-        printf("\n O aluno %d est· APROVADO com mÈdia: %.2f ",i,media);
+        printf("\n O aluno %d est√° APROVADO com m√©dia: %.2f ",i,media);
         printf("\n ***************************** \n");
         }
 
         else{
-        printf("\n O aluno %d est· REPROVADO com mÈdia: %.2f ",i,media);
+        printf("\n O aluno %d est√° REPROVADO com m√©dia: %.2f ",i,media);
         printf("\n ***************************** \n");
         }
     }
