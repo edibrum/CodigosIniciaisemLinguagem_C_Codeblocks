792727bee5a26e472dce834e4057bbdd0f24cb4d
Edineia Brum (she/her/hers) <106160411+edibrum@users.noreply.github.com>
2022-08-30 10:07:10 -0300

    Renomeando arquivos 068

--- 068_le_20_numeros_e_mostra_os_que_estao_entre_0_e_100_main.c
+++ 068_le_20_numeros_e_mostra_os_que_estao_entre_0_e_100_main.c
@@ -2,15 +2,15 @@
 #include <stdlib.h>
 
 /*LISTA 3
-11) FaÁa um algoritmo que leia 20 n˙meros e, ao final, escreva quantos est„o entre 0 e 100. */
+11) Fa√ßa um algoritmo que leia 20 n√∫meros e, ao final, escreva quantos est√£o entre 0 e 100. */
 
 int main()
 {
-    /* Declara uma vari·vel acumuladora que vai contar quantos n˙meros est„o entre 0 e 100, e a vari·vel n˙mero que vai receber cada valor digitado: */
+    /* Declara uma vari√°vel acumuladora que vai contar quantos n√∫meros est√£o entre 0 e 100, e a vari√°vel n√∫mero que vai receber cada valor digitado: */
     int quantos_num=0,numero;
 
 
-    /* Pede ao usu·rio que digite os 20 n˙meros: */
+    /* Pede ao usu√°rio que digite os 20 n√∫meros: */
     for (int i=1;i<=20;i++)
     {
         printf("Digite o numero %d: \n",i);
@@ -22,7 +22,7 @@ int main()
         }
     }
 
-    /* Retorna o resultado para o usu·rio: */
+    /* Retorna o resultado para o usu√°rio: */
         printf("Dentre os 20 numeros que voce digitou, ha um total de %d que estao entre 0 e 100.",quantos_num);
 
 
