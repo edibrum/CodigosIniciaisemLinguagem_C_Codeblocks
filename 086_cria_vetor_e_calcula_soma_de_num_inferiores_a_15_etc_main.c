0fe720ad430f2ac94ad9d7010580a87505cd8c61
Edineia Brum (she/her/hers) <106160411+edibrum@users.noreply.github.com>
2022-08-30 17:21:48 -0300

    Renomeando arquivos 086

--- 086_cria_vetor_e_calcula_soma_de_num_inferiores_a_15_etc_main.c
+++ 086_cria_vetor_e_calcula_soma_de_num_inferiores_a_15_etc_main.c
@@ -3,21 +3,21 @@
 #include <locale.h>
 /*LISTA 4
 5) Criar um vetor A com 10 elementos inteiros. Escrever um programa que calcule e escreva:
-a) a soma de elementos armazenados neste vetor que s„o inferiores a 15;
-b) a quantidade de elementos armazenados no vetor que s„o iguais a 15; e
-c) a mÈdia dos elementos armazenados no vetor que s„o superiores a 15.
+a) a soma de elementos armazenados neste vetor que s√£o inferiores a 15;
+b) a quantidade de elementos armazenados no vetor que s√£o iguais a 15; e
+c) a m√©dia dos elementos armazenados no vetor que s√£o superiores a 15.
 */
 
 int main() {
     setlocale(LC_ALL, "Portuguese");
 
-    /* Declara as vari·veis: */
+    /* Declara as vari√°veis: */
     int TAM=10,numero,menor_15=0,igual_15=0,maior_15=0,soma=0;
     float media;
     int vetor[TAM];
 
     for(int i=0;i<TAM;i++){
-        printf("Digite o n˙mero %d do vetor:",i+1);
+        printf("Digite o n√∫mero %d do vetor:",i+1);
         scanf("%d",&numero);
         vetor[i]=numero;
 
@@ -45,7 +45,7 @@ int main() {
     }
 
     /*Mostra os resultados: */
-    printf("\n A soma dos n˙meros menores que 15 È: %d \n A mÈdia dos n˙meros maiores que 15 È: %.2f \n A quantidade de ocorrÍncias do n˙mero 15 È: %d \n",soma,(media/maior_15),igual_15);
+    printf("\n A soma dos n√∫meros menores que 15 √©: %d \n A m√©dia dos n√∫meros maiores que 15 √©: %.2f \n A quantidade de ocorr√™ncias do n√∫mero 15 √©: %d \n",soma,(media/maior_15),igual_15);
 
 
     return 0;
