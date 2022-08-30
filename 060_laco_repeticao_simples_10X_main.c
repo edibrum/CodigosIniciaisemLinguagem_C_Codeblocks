68c1fd8c9ced68393e4407f9e87cad7054a1cfda
Edineia Brum (she/her/hers) <106160411+edibrum@users.noreply.github.com>
2022-08-30 09:57:44 -0300

    Renomeando arquivos 060

--- 060_laco_repeticao_simples_10X_main.c
+++ 060_laco_repeticao_simples_10X_main.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 /*LISTA 3
-3) Leia o nome do usu·rio e escreva o nome dele na tela 10 vezes. */
+3) Leia o nome do usu√°rio e escreva o nome dele na tela 10 vezes. */
 
 int main()
 {
     int n=0;
     char nome[40];
 
-    /* Pede ao usu·rio que digite o nome: */
+    /* Pede ao usu√°rio que digite o nome: */
     printf("Digite seu nome completo (para espacos use '_': \n");
     scanf(" %s",nome);
 
-    /* Mostra o nome do usu·rio 10 vezes: */
+    /* Mostra o nome do usu√°rio 10 vezes: */
     for (int i=1;i<=10;i++)
     {
         n=n+1;
