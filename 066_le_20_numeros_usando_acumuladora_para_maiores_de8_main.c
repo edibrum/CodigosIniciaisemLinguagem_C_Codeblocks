6bb18063ccc5199d4c0d8179dd99cea34d223ed3
Edineia Brum (she/her/hers) <106160411+edibrum@users.noreply.github.com>
2022-08-30 10:04:31 -0300

    Renomeando arquivos 066

--- 066_le_20_numeros_usando_acumuladora_para_maiores_de8_main.c
+++ 066_le_20_numeros_usando_acumuladora_para_maiores_de8_main.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 /*LISTA 3
-9)Escreva um algoritmo que leia 20 n˙meros informados pelo usu·rio e exiba quantos n˙meros s„o maiores do que 8. */
+9)Escreva um algoritmo que leia 20 n√∫meros informados pelo usu√°rio e exiba quantos n√∫meros s√£o maiores do que 8. */
 
 int main()
 {
-    /* Declara uma a vari·vel acumuladora que vai contar quantos n˙meros ser„o maiores que 8, e a vari·vel n˙mero que vai receber cada valor digitado: */
+    /* Declara uma a vari√°vel acumuladora que vai contar quantos n√∫meros ser√£o maiores que 8, e a vari√°vel n√∫mero que vai receber cada valor digitado: */
     int quantos_num=0,numero;
 
 
-    /* Pede ao usu·rio que digite os 20 n˙meros: */
+    /* Pede ao usu√°rio que digite os 20 n√∫meros: */
     for (int i=1;i<=20;i++)
     {
         printf("Digite o numero %d: \n",i);
@@ -21,7 +21,7 @@ int main()
         }
     }
 
-    /* Retorna o resultado para o usu·rio: */
+    /* Retorna o resultado para o usu√°rio: */
         printf("Dentre os 20 numeros que voce digitou, ha um total de %d numeros maiores que 8.",quantos_num);
 
 
