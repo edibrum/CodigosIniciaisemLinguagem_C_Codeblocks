3410caa7185ad107b6973947cc2837c2ee0d698d
Edineia Brum (she/her/hers) <106160411+edibrum@users.noreply.github.com>
2022-08-30 10:06:22 -0300

    Renomeando arquivos 067

--- 067_le_20_numeros_e_mostra_os_pares_main.c
+++ 067_le_20_numeros_e_mostra_os_pares_main.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 /*LISTA 3
-10) Escreva um algoritmo que leia 20 n˙meros informados pelo usu·rio e exiba quantos n˙meros s„o pares. */
+10) Escreva um algoritmo que leia 20 n√∫meros informados pelo usu√°rio e exiba quantos n√∫meros s√£o pares. */
 
 int main()
 {
-    /* Declara uma a vari·vel acumuladora que vai contar quantos n˙meros ser„o pares, e a vari·vel n˙mero que vai receber cada valor digitado: */
+    /* Declara uma a vari√°vel acumuladora que vai contar quantos n√∫meros ser√£o pares, e a vari√°vel n√∫mero que vai receber cada valor digitado: */
     int quantos_num=0,numero;
 
 
-    /* Pede ao usu·rio que digite os 20 n˙meros: */
+    /* Pede ao usu√°rio que digite os 20 n√∫meros: */
     for (int i=1;i<=20;i++)
     {
         printf("Digite o numero %d: \n",i);
@@ -21,6 +21,6 @@ int main()
         }
     }
 
-    /* Retorna o resultado para o usu·rio: */
+    /* Retorna o resultado para o usu√°rio: */
         printf("Dentre os 20 numeros que voce digitou, ha um total de %d numeros pares.",quantos_num);
 }
