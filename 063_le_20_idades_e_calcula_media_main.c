fc59b69aa85003adb1857092b76c51b428e361b6
Edineia Brum (she/her/hers) <106160411+edibrum@users.noreply.github.com>
2022-08-30 10:00:50 -0300

    Renomeando arquivos 063

--- 063_le_20_idades_e_calcula_media_main.c
+++ 063_le_20_idades_e_calcula_media_main.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 /*LISTA 3
-6) Leia a idade de 20 pessoas e exiba a mÈdia das idades. */
+6) Leia a idade de 20 pessoas e exiba a m√©dia das idades. */
 
 int main()
 {
-    /*Cria as vari·veis: */
+    /*Cria as vari√°veis: */
     int idade,total;
 
-    /*Pede que o usu·rio digite os valores para as vari·veis de entrada e calcula: */
+    /*Pede que o usu√°rio digite os valores para as vari√°veis de entrada e calcula: */
     for (int i=1;i<=20;i++)
     {
     printf("\n Digite a idade da PESSOA %d:  ",i);
