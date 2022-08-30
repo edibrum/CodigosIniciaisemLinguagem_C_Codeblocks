c14cb64bfbafa059f03551267b481cede72e0248
Edineia Brum (she/her/hers) <106160411+edibrum@users.noreply.github.com>
2022-08-30 10:01:46 -0300

    Renomeando arquivos 064

--- 064_le_20_idades_e_mostra_maiores_de_18_main.c
+++ 064_le_20_idades_e_mostra_maiores_de_18_main.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 /*LISTA 3
-7) Leia a idade de 20 pessoas e exiba quantas pessoas s„o maiores de idade. */
+7) Leia a idade de 20 pessoas e exiba quantas pessoas s√£o maiores de idade. */
 
 int main()
 {
-    /*Cria as vari·veis: */
+    /*Cria as vari√°veis: */
     int idade,maiores=0;
 
-    /*Pede que o usu·rio digite as idades e contabiliza qunado È maior de idade: */
+    /*Pede que o usu√°rio digite as idades e contabiliza qunado √© maior de idade: */
     for (int i=1;i<=20;i++)
     {
     printf("\n Digite a idade da PESSOA %d:  ",i);
@@ -21,7 +21,7 @@ int main()
             }
     }
 
-    /*Mostra quantas pessoas s„o maiores: */
+    /*Mostra quantas pessoas s√£o maiores: */
     printf("\n ************* \n Sao %d pessoas maiores de idade. \n",maiores);
 
     return 0;
