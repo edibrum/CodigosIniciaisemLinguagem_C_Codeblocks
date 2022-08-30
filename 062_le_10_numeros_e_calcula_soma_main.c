54f83a018a2c172ba680b292c1e7d9994acb4f76
Edineia Brum (she/her/hers) <106160411+edibrum@users.noreply.github.com>
2022-08-30 10:00:07 -0300

    Renomeando arquivos 062

--- 062_le_10_numeros_e_calcula_soma_main.c
+++ 062_le_10_numeros_e_calcula_soma_main.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 /*LISTA 3
-5) Escreva um algoritmo que leia 10 n˙meros informados pelo usu·rio e calcule a soma desses n˙meros. */
+5) Escreva um algoritmo que leia 10 n√∫meros informados pelo usu√°rio e calcule a soma desses n√∫meros. */
 
 int main()
 {
-   /*Cria as vari·veis: */
+   /*Cria as vari√°veis: */
     float numero,somatorio=0.00;
 
-    /*Pede que o usu·rio digite os valores para as vari·veis de entrada e calcula: */
+    /*Pede que o usu√°rio digite os valores para as vari√°veis de entrada e calcula: */
     for (int contador=1;contador<=10;contador++)
     {
     printf("\n Digite o numero %d:  ",contador);
