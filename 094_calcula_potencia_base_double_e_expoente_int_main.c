7d11c3ee81096a3abc8d3b5c42c4f9ad3e921cab
Edineia Brum (she/her/hers) <106160411+edibrum@users.noreply.github.com>
2022-09-01 15:20:41 -0300

    Renomeando arquivos 094

--- 094_calcula_potencia_base_double_e_expoente_int_main.c
+++ 094_calcula_potencia_base_double_e_expoente_int_main.c
@@ -2,8 +2,8 @@
 #include <stdlib.h>
 #include <locale.h>
 
-/*Escreva uma função que computa a potência a^b para valores a (double) e b (int) passados por parâmetro
-(não use bibliotecas como math.h). Use a função anterior e crie um programa que imprima todas as potências:
+/*Escreva uma funÃ§Ã£o que computa a potÃªncia a^b para valores a (double) e b (int) passados por parÃ¢metro
+(nÃ£o use bibliotecas como math.h). Use a funÃ§Ã£o anterior e crie um programa que imprima todas as potÃªncias:
 2^0, 2^1, ..., 2^10, 3^0, ..., 3^10, ..., 10^10. */
 
 void calcula_potencia(double base, int expoente);
@@ -15,7 +15,7 @@ int main()
     double base;
     int expoente;
 
-    printf("\n VAMOS CALCULAR AS DEZ PRIMEIRAS POTÊNCIAS DE UM NÚMERO INTEIRO \n");
+    printf("\n VAMOS CALCULAR AS DEZ PRIMEIRAS POTÃŠNCIAS DE UM NÃšMERO INTEIRO \n");
     calcula_potencia(base,expoente);
 
     return 0;
@@ -28,7 +28,7 @@ void calcula_potencia(double base, int expoente){
     scanf("%lf",&base);
 
     expoente==0;
-    printf("\n POTÊNCIAS DO NÚMERO %.2lf \n %.2lf ^ %d = %d",base,base,expoente,potencia);
+    printf("\n POTÃŠNCIAS DO NÃšMERO %.2lf \n %.2lf ^ %d = %d",base,base,expoente,potencia);
 
     for(int expoente=1;expoente<=10;expoente++){
 
